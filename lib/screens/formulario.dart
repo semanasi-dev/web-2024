8bb46be192eaa7c782a2046874f7707e66fd3474
DanielBrisch <danielbrisch157@gmail.com>
2024-06-04 01:41:47 -0300

    fazendo o começo da tela de formulario

--- lib/screens/formulario.dart
+++ lib/screens/formulario.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 
 class Formulario extends StatefulWidget {
   const Formulario({super.key});
@@ -13,49 +15,136 @@ class FformularioState extends State<Formulario> {
     Size size = MediaQuery.of(context).size;
 
     return Scaffold(
-      backgroundColor: Colors.black,
       body: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(
-            colors: [Colors.blue, Colors.white, Colors.deepPurple],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
+            colors: [Color(0xFFbc3764), Color.fromARGB(255, 122, 119, 216)],
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
           ),
         ),
-        child: Center(
-          child: SingleChildScrollView(
-            child: Center(
+        child: SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: size.height,
+            ),
+            child: IntrinsicHeight(
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Container(
-                    width: size.width * 0.4,
-                    height: size.height * 0.7,
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.5),
-                            spreadRadius: 3,
-                            blurRadius: 7,
-                            offset: const Offset(0, 3),
+                  Padding(
+                    padding: EdgeInsets.only(
+                        top: size.height * 0.04, bottom: size.height * 0.03),
+                    child: Container(
+                      height: size.height * 0.3,
+                      width: size.width * 0.8,
+                      decoration: BoxDecoration(
+                          image: const DecorationImage(
+                              opacity: 0.5,
+                              image: AssetImage(
+                                './lib/assets/andinho.jpg',
+                              ),
+                              fit: BoxFit.fill),
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Image.asset(
+                        'lib/assets/logo.png',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: size.width * 0.4,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              verticalDirection: VerticalDirection.down,
+                              children: [
+                                const Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Text(
+                                    'Semana Acadêmica',
+                                    style: TextStyle(
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white),
+                                  ),
+                                ),
+                                Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    containerPergunta(
+                                        'Nome', 'Digite seu nome', size),
+                                    containerPergunta(
+                                        'CPF', 'Digite seu CPF', size),
+                                    containerPergunta('Linkedin',
+                                        'Digite seu Linkedin', size),
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
                         ],
-                        borderRadius: BorderRadius.circular(15)),
-                    child: const Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      verticalDirection: VerticalDirection.down,
-                      children: [
-                        Text("Formulário"),
-                        TextField(),
-                      ],
+                      ),
                     ),
-                  )
+                  ),
                 ],
               ),
             ),
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget containerPergunta(String nomeDoCampo, String hint, Size size) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20, top: 20),
+      child: Container(
+        height: size.height * 0.15,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.5),
+              spreadRadius: 3,
+              blurRadius: 7,
+              offset: const Offset(0, 3),
+            ),
+          ],
+          borderRadius: BorderRadius.circular(15),
+          gradient: const LinearGradient(
+            colors: [Color(0xFF352841), Color(0xFF5b4770)],
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  nomeDoCampo,
+                  style: const TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: hint,
+                    hintStyle: const TextStyle(color: Colors.grey),
+                    filled: true,
+                    fillColor: const Color(0xFF64527a),
+                    border:
+                        const OutlineInputBorder(borderSide: BorderSide.none),
+                  ),
+                ),
+              ]),
         ),
       ),
     );

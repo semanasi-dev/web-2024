f42f7802a8f030526fb0b1020d5cd78fea1f9eea
DanielBrisch <danielbrisch157@gmail.com>
2024-06-08 09:10:05 -0300

    fix: melhorando responsividade do formulario

--- lib/screens/formulario.dart
+++ lib/screens/formulario.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'widgets/nav_bar.dart';
 
 class Formulario extends StatefulWidget {
@@ -23,7 +22,7 @@ class FormularioState extends State<Formulario> {
             return telaTamanhoMaximo();
           } else if (constraints.maxWidth > 800 &&
               constraints.maxWidth <= 1200) {
-            return telaTamnhoMedio();
+            return telaTamanhoMedio();
           } else {
             return telaTamanhoPequeno();
           }
@@ -32,53 +31,35 @@ class FormularioState extends State<Formulario> {
     );
   }
 
-  Widget telaTamanhoMaximo() {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(40),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Flexible(flex: 1, child: leftColumn()),
-            const SizedBox(width: 20),
-            Flexible(flex: 1, child: rightContainer()),
-          ],
-        ),
-      ),
-    );
-  }
+  Widget telaTamanhoMaximo() =>
+      buildResponsiveLayout(40, MainAxisAlignment.spaceBetween);
+  Widget telaTamanhoMedio() =>
+      buildResponsiveLayout(20, MainAxisAlignment.center);
+  Widget telaTamanhoPequeno() =>
+      buildResponsiveLayout(10, MainAxisAlignment.center, isSmall: true);
 
-  Widget telaTamnhoMedio() {
+  Widget buildResponsiveLayout(double padding, MainAxisAlignment alignment,
+      {bool isSmall = false}) {
     return SingleChildScrollView(
       child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Expanded(child: leftColumn()),
-                const SizedBox(width: 20),
-                Expanded(child: rightContainer()),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget telaTamanhoPequeno() {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          children: [
-            leftColumn(),
-            const SizedBox(height: 20),
-            rightContainer(),
-          ],
-        ),
+        padding: EdgeInsets.all(padding),
+        child: isSmall
+            ? Column(
+                children: [
+                  leftColumn(),
+                  const SizedBox(height: 20),
+                  rightContainer(),
+                ],
+              )
+            : Row(
+                mainAxisAlignment: alignment,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Flexible(flex: 1, child: leftColumn()),
+                  SizedBox(width: isSmall ? 10 : 20),
+                  Flexible(flex: 1, child: rightContainer()),
+                ],
+              ),
       ),
     );
   }
@@ -86,90 +67,58 @@ class FormularioState extends State<Formulario> {
   Widget leftColumn() {
     return Column(
       children: [
-        Container(
-          height: 700,
-          width: double.infinity,
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-              color: Colors.grey,
-              border: Border.all(color: Colors.black, width: 2.0),
-              borderRadius: BorderRadius.circular(10)),
-          child: const Text(
-            'Área de texto sobre a semana academica',
-            style: TextStyle(color: Colors.black, fontSize: 30),
-          ),
-        ),
+        buildContainer('Área de texto sobre a semana acadêmica', 700,
+            onTap: () {}),
         const SizedBox(height: 20),
-        SizedBox(
-          height: 50,
-          width: 300,
-          child: GestureDetector(
-            onTap: () {},
-            child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.grey,
-                  border: Border.all(color: Colors.black, width: 2.0),
-                  borderRadius: BorderRadius.circular(10)),
-              alignment: Alignment.center,
-              child: const Text(
-                'Quero participar',
-                style: TextStyle(color: Colors.white, fontSize: 18.0),
-              ),
-            ),
-          ),
-        ),
+        buildButton('Quero participar', onTap: () {}),
         const SizedBox(height: 20),
-        SizedBox(
-          height: 50,
-          width: 300,
-          child: GestureDetector(
-            onTap: () {},
-            child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.grey,
-                  border: Border.all(color: Colors.black, width: 2.0),
-                  borderRadius: BorderRadius.circular(10)),
-              alignment: Alignment.center,
-              child: const Text(
-                'Ver mais',
-                style: TextStyle(color: Colors.white, fontSize: 18.0),
-              ),
-            ),
-          ),
-        ),
+        buildButton('Ver mais', onTap: () {}),
       ],
     );
   }
 
   Widget rightContainer() {
+    return buildContainer('fodaseeeeeeeeeee', 800);
+  }
+
+  Widget buildContainer(String text, double height, {Function()? onTap}) {
     return Container(
-      height: 800,
+      height: height,
       width: double.infinity,
       alignment: Alignment.center,
       decoration: BoxDecoration(
-          color: Colors.grey,
-          border: Border.all(color: Colors.black, width: 2.0),
-          borderRadius: BorderRadius.circular(10)),
-      child: const Text(
-        'fodaseeeeeeeeeee',
-        style: TextStyle(color: Colors.black),
+        color: Colors.grey,
+        border: Border.all(color: Colors.black, width: 2.0),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: GestureDetector(
+        onTap: onTap,
+        child: Text(
+          text,
+          style: const TextStyle(color: Colors.black, fontSize: 18.0),
+        ),
       ),
     );
   }
-}
 
-class NotFoundPage extends StatelessWidget {
-  const NotFoundPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(60.0),
-        child: NavBar(),
-      ),
-      body: Center(
-        child: Text('404 Page Not Found'),
+  Widget buildButton(String text, {Function()? onTap}) {
+    return SizedBox(
+      height: 50,
+      width: 300,
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.grey,
+            border: Border.all(color: Colors.black, width: 2.0),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          alignment: Alignment.center,
+          child: Text(
+            text,
+            style: const TextStyle(color: Colors.white, fontSize: 18.0),
+          ),
+        ),
       ),
     );
   }

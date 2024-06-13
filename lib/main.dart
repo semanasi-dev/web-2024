a3118ddb38e703b293fab587f888e16329218de4
DanielBrisch <danielbrisch157@gmail.com>
2024-06-13 00:15:26 -0300

    gradient do diabo

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,88 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
-import 'package:semanaacademica2024/screens/Inscricoes_page.dart';
-import 'package:semanaacademica2024/screens/home_page.dart';
 
-void main() {
-  setUrlStrategy(PathUrlStrategy());
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      onGenerateRoute: (settings) {
-        WidgetBuilder builder;
-        switch (settings.name) {
-          case '/':
-            builder = (BuildContext _) => const HomePage();
-            break;
-          case '/inscricoes':
-            builder = (BuildContext _) => const Inscricoes();
-            break;
-          default:
-            throw Exception('Rota não definida: ${settings.name}');
-        }
-        return createRoute(builder(context));
-      },
+      home: Scaffold(
+        body: ListView(
+          children: <Widget>[
+            Stack(
+              children: [
+                Image.asset(
+                  './lib/assets/parallax-top.jpeg',
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+                Positioned.fill(
+                  top: 450,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [Colors.transparent, Colors.blue],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            // Segundo bloco com gradiente e imagem
+            Container(
+              height: 200,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.blue, Colors.white.withOpacity(0.9)],
+                ),
+              ),
+            ),
+            Container(
+              height: 200,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.white, Colors.blue.withOpacity(0.9)],
+                ),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.white, Colors.transparent],
+                ),
+              ),
+            ),
+            Stack(children: [
+              Image.asset(
+                './lib/assets/background-purple.jpg', // Substitua pelo caminho da sua segunda imagem
+                height: 400,
+                fit: BoxFit.cover,
+              ),
+              Positioned.fill(
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [Colors.blue, Colors.transparent],
+                    ),
+                  ),
+                ),
+              ),
+            ]),
+          ],
+        ),
+      ),
     );
   }
-}
-
-Route createRoute(Widget page) {
-  return PageRouteBuilder(
-    pageBuilder: (context, animation, secondaryAnimation) => page,
-    transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      return child;
-    },
-  );
 }
